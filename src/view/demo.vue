26e19b2b2b43d2f07b378370ee31dc1d522fa6c3
Milk <2582789473@qq.com>
2018-08-24 16:46:58 +0800

    初始化

---
+++ src/view/demo.vue
@@ -0,0 +1,25 @@
+<template>
+	<section>
+		<button @click="dialogVisible = true">点我上传头像</button>
+		<uploadAvatar :dialogVisible="dialogVisible" @on-close="closeDialog"></uploadAvatar>
+	</section>
+</template>
+
+<script>
+	import uploadAvatar from "@/components/uploadAvatar.vue"
+	export default {
+		data() {
+			return {
+				dialogVisible: false,
+			}
+		},
+		components: {
+			uploadAvatar
+		},
+		methods: {
+			closeDialog() {
+				this.dialogVisible = false
+			}
+		}
+	};
+</script>
